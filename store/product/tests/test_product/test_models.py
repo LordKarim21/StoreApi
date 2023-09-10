b71807598d983255727bf91735a65fe6eade4552
Карим Исмаил <ismail.karim01@inbox.ru>
2023-09-11 00:11:32 +0600

    corrected model Product and Order, add tests view corrected reviews

---
+++ store/product/tests/test_product/test_models.py
@@ -0,0 +1,26 @@
+from order.models import Order
+from user.models import User
+from product.models import Basket
+
+from django.test import TestCase
+
+
+class TestCaseModulOrder(TestCase):
+    def setUp(self) -> None:
+        user_data = {
+            "password": "testAdmin",
+            "username": "testAdmin",
+            }
+        self.user = User.objects.create_user(user_data)
+        self.order = Order.objects.create(initiator=self.user, first_name="test", last_name="test",
+                                          email="test@test.test", address="testAddress", total_cost=12.00)
+
+    def test_exists_user_and_order(self):
+        self.assertEqual(self.user.id, 1)
+        self.assertEqual(self.order.id, 1)
+
+    def test_update_after_payment(self):
+        self.assertEqual(self.order.status, 'o')
+        self.order.update_after_payment()
+        self.assertEqual(self.order.status, 'p')
+        self.assertEqual(Basket.objects.filter(user=self.user).count(), 0)
